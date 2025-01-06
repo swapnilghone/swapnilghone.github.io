09bc910ed2c6b72309c0894a68e7ef3fd394e88c
Swap <swapnilghone9@gmail.com>
2025-01-06 20:32:02 +0530

    feat: about section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,47 +10,52 @@ const About = () => {
                 </div>
 
                 <div className="row">
-                    <div className="col-lg-12 mb-5">
+                    <div className="col-lg-12 mb-4">
                         <h4 className="mb-4 heading">Seasoned Development and Project Management Professional</h4>
                         <ul>
-                            <li>
-                                Over 10 years of hands-on experience in software development,
-                                project management, and team leadership, encompassing all stages
-                                of the software lifecycle.
+                            <li><i class="fa fa-chevron-right"></i>
+                                Over 10 years of extensive experience in software development, project management, and team
+                                leadership.
                             </li>
-                            <li>
-                                Successfully managed projects for clients worldwide, including
-                                the United States, Germany, Australia, and Japan, demonstrating
-                                adaptability to diverse cultural and business environments.
+                            <li><i class="fa fa-chevron-right"></i>
+                                Comprehensive knowledge of the software development lifecycle, from planning and design to
+                                deployment and maintenance.
                             </li>
                         </ul>
                     </div>
-                    <div className="col-lg-12 mb-5">
+                    <div className="col-lg-12 mb-4">
                         <h4 className="mb-4 heading">Expertise in Full Stack Development and Technical Leadership</h4>
                         <ul>
-                            <li>
-                                Proficient in a wide range of technologies, including PHP, MySQL,
-                                JavaScript, WordPress, Laravel, ReactJs, and Git, showcasing a
-                                versatile skill set.
+                            <li><i class="fa fa-chevron-right"></i>
+                                Proven expertise in designing and developing scalable web applications, microservice-based APIs, and modular systems using PHP, JavaScript, TypeScript, ReactJS, Node.js, Laravel, and WordPress.
                             </li>
-                            <li>
-                                Led and mentored development teams, fostering collaboration
-                                and innovation, resulting in successful project deliveries and client
-                                satisfaction.
+                            <li><i class="fa fa-chevron-right"></i>
+                                Strong understanding of MVC architecture and Object-Oriented Programming (OOP) principles,
+                                with a focus on clean, maintainable code.
+                            </li>
+                            <li><i class="fa fa-chevron-right"></i>
+                                Extensive experience in WordPress block development, WordPress Multisite setups, and
+                                optimizing site speed and performance to deliver seamless user experiences.
+                            </li>
+                            <li><i class="fa fa-chevron-right"></i>
+                                Specialized in plugin development and customization, with hands-on expertise in WordPress REST API and poluplar WordPress plugins like WooCommerce,
+                                WPML, Elementor, Gravity Forms.
+                            </li>
+                            <li><i class="fa fa-chevron-right"></i>
+                                Proficient in cloud services, including Amazon EC2, S3, and Lambda
                             </li>
                         </ul>
                     </div>
                     <div className="col-lg-12">
                         <h4 className="mb-4 heading">Proven Track Record in Project Execution and Client Engagement</h4>
                         <ul>
-                            <li>
-                                Executed and delivered multiple projects with a focus on precision
-                                and quality, meeting or exceeding client expectations.
+                            <li><i class="fa fa-chevron-right"></i>
+                                Demonstrated ability to manage cross-functional teams, mentor developers, and oversee projects in
+                                e-commerce, MLM systems, Healthcare, TravelTech and EdTech domains.
                             </li>
-                            <li>
-                                Established effective communication channels with clients,
-                                ensuring a deep understanding of their requirements and providing
-                                tailored solutions.
+                            <li><i class="fa fa-chevron-right"></i>
+                                Successfully collaborated with international clients from the United States, Germany, Australia,
+                                and Japan, delivering high-quality, customized solutions.
                             </li>
                         </ul>
                     </div>
